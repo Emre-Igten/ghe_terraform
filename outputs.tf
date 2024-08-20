784cb4db4b3ee2c7fa455ffe123c302639bd2c57
Emre-Igten <emre.igten@student.hogent.be>
2024-08-20 15:09:49 +0200

    fix2

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "organization_name" {
-  description = "Name of the created GitHub organization"
-  value       = module.create-organization.organization_name
-}
+# output "organization_name" {
+#   description = "Name of the created GitHub organization"
+#   value       = module.create-organization.organization_name
+# }
 
 
-output "secret_value" {
-  value = data.github_actions_secret.admin_token.value
-}
+# output "secret_value" {
+#   value = data.github_actions_secret.admin_token.value
+# }
