2dd993681e062bc7e45839af8f9f7b117a925f21
Emre-Igten <emre.igten@student.hogent.be>
2024-08-20 15:16:26 +0200

    fix3

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "admin_logins" {
-  description = "The admin logins of the organization"
-  type        = list(string)
-  default = ["igtene_dlwr"]
-}
-
-variable "billing_email" {
-  description = "The billing email of the organization"
-  type        = string
-  default = "joke.roelstraete@delaware.pro"
-}
-
 
 
 
